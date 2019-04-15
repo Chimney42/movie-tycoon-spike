55fd12608957b143672b697323977886e0b1239e
lian <draggili@gmail.com>
2019-04-15 12:13:08 +0200

    adds rating

--- src/models/screenplay.ts
+++ src/models/screenplay.ts
@@ -2,10 +2,19 @@ import Genre from './genre';
 class Screenplay {
     readonly id: number;
     readonly genre: Genre;
+    private rating: number|null = null;
 
     constructor(id: number, genre: Genre) {
         this.id = id;
         this.genre = genre;
+    }
+
+    setRating(rating: number) {
+        this.rating = rating;
+    }
+
+    getRating(): number|null {
+        return this.rating;
     }
 }
 
--- tests/models/screenplay.spec.ts
+++ tests/models/screenplay.spec.ts
@@ -3,11 +3,23 @@ import Genre from '../../src/models/genre';
 import { expect } from 'chai';
 
 describe('A screenplay', () => {
-    it('should have an id and a genre', () => {
         const id = 213;
         const genre = Genre.Action;
-        const screenplay = new Screenplay(id, genre);
+        let screenplay: Screenplay;
+    beforeEach(() => {
+        screenplay = new Screenplay(id, genre);
+    })
+
+    it('should have an id and a genre', () => {
         expect(screenplay.id).to.equal(id);
         expect(screenplay.genre).to.equal(genre);
     });
+
+    it('should have a settable rating', () => {
+        const rating = 5;
+        
+        expect(screenplay.getRating()).not.to.equal(rating);
+        screenplay.setRating(rating);
+        expect(screenplay.getRating()).to.equal(rating);
+    })
   });
