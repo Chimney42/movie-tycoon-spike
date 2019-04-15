54ee7bb87f4c19ce429d465a6f1c9b4a306080c3
lian <draggili@gmail.com>
2019-04-15 12:37:13 +0200

    director rating

--- src/models/director.ts
+++ src/models/director.ts
@@ -1,10 +1,19 @@
 class Director {
   readonly id: number;
   readonly name: String;
+  private rating: number|null = null;
 
   constructor(id: number, name: String) {
     this.id = id;
     this.name = name;
+  }
+
+  setRating(rating: number) {
+    this.rating = rating;
+  }
+
+  getRating(): number|null {
+    return this.rating;
   }
 }
 
--- tests/models/director.spec.ts
+++ tests/models/director.spec.ts
@@ -2,12 +2,24 @@ import Director from '../../src/models/director';
 import { expect } from 'chai';
 
 describe('A director', () => {
-  it('should have an id and a name', () => {
-    const id = 231;
-    const name = 'Foo Bar';
-    const director = new Director(id, name);
+  const id = 231;
+  const name = 'Foo Bar';
+  let director: Director;
 
+  beforeEach(() => {
+    director = new Director(id, name);
+  })
+
+  it('should have an id and a name', () => {
     expect(director.id).to.equal(id);
     expect(director.name).to.equal(name);
-  })
+  });
+
+  it('should have a settable rating', () => {
+    const rating = 3;
+
+    expect(director.getRating()).not.to.equal(rating);
+    director.setRating(rating);
+    expect(director.getRating()).to.equal(rating);
+  });
 });
