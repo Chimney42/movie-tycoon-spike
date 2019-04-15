11f338227caf3f53d7b7fdda4806cf80e6538a44
lian <draggili@gmail.com>
2019-04-15 12:19:22 +0200

    formatting

--- tests/models/screenplay.spec.ts
+++ tests/models/screenplay.spec.ts
@@ -6,9 +6,10 @@ describe('A screenplay', () => {
         const id = 213;
         const genre = Genre.Action;
         let screenplay: Screenplay;
+        
     beforeEach(() => {
         screenplay = new Screenplay(id, genre);
-    })
+    });
 
     it('should have an id and a genre', () => {
         expect(screenplay.id).to.equal(id);
@@ -17,9 +18,9 @@ describe('A screenplay', () => {
 
     it('should have a settable rating', () => {
         const rating = 5;
-        
+
         expect(screenplay.getRating()).not.to.equal(rating);
         screenplay.setRating(rating);
         expect(screenplay.getRating()).to.equal(rating);
-    })
+    });
   });
